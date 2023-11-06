e810eefea92f15b7012729e5dbe663249ddf41b9
Bruno Jose Silva <brunojs@gmail.com>
2023-11-06 20:01:26 -0300

    Exercicio ts

---
+++ exercicio_ts.ts
@@ -0,0 +1,11 @@
+function ResultadoAB (numeroA: number, numeroB: number): number {
+    return numeroA * numeroB;
+}
+
+console.log(ResultadoAB(10, 20));
+
+function Saudacoes (nome: string): string {
+    return `Ola ${nome}`;
+}
+
+console.log(Saudacoes);
